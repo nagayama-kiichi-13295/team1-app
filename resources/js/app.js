198f74f984071204491549106c36faa7b7d56b12
kiichi1094 <k.nagayama@tbc-inc.co.jp>
2026-07-28 14:24:51 +0900

    名前入力画面CSS変更

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,6 +3,7 @@ import './echo';
 
 import '../css/battle.css';
 import '../css/character.css';
+import '../css/player.css';
 
 console.log('APP LOADED');
 console.log(window.Echo);
--- resources/views/player.blade.php
+++ resources/views/player.blade.php
@@ -1,25 +1,66 @@
 <!DOCTYPE html>
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>プレイヤー名入力</title>
+
+    @vite([
+        'resources/js/app.js'
+    ])
 </head>
-<body>
+<body class="player-page">
 
-    <h1>バケモンバトル2</h1>
+    <div class="title-panel">
 
-    <form action="/player" method="POST">
-        @csrf
+        <h1 class="game-title">
+            バケモン<span class="title-accent">バトル</span>2
+        </h1>
 
-        <input
-            type="text"
-            name="player_name"
-            placeholder="プレイヤー名"
-        >
+        <p class="game-subtitle">
+            BAKEMON BATTLE
+        </p>
 
-        <button type="submit">
-            開始
-        </button>
-    </form>
+        {{-- バリデーションエラー --}}
+        @if($errors->any())
+
+            <div class="error-box">
+
+                @foreach($errors->all() as $error)
+
+                    <p>{{ $error }}</p>
+
+                @endforeach
+
+            </div>
+
+        @endif
+
+        <form action="/player" method="POST">
+            @csrf
+
+            <label class="name-label" for="player_name">
+                プレイヤー名
+            </label>
+
+            <input
+                id="player_name"
+                class="name-input"
+                type="text"
+                name="player_name"
+                value="{{ old('player_name') }}"
+                placeholder="名前を入力"
+                maxlength="50"
+                autocomplete="off"
+            >
+
+            <button type="submit" class="start-btn">
+                ゲーム開始
+            </button>
+
+        </form>
+
+    </div>
 
 </body>
 </html>
